39257533dc2a809cfe9ccc25c2c72516f03bf0d3
jisu <ssu7358@naver.com>
2024-12-16 22:43:05 +0900

    [feat] #3.2 Card Component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,9 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: []
 };
 export default config;
