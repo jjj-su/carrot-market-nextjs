d720e783ee4c9b3be612f3ae7a3a5c568bce6755
jisu <ssu7358@naver.com>
2024-12-26 01:52:44 +0900

    [feat] #8.10 Matcher

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import getSession from "@/lib/session";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+};
